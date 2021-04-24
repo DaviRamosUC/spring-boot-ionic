295c6624f66fbc51da7167fa89b36b0cff26eb82
Davi <davi.lima@ucsal.edu.br>
2021-04-24 13:11:44 -0300

    Estado e Cidade

--- src/main/java/com/devdavi/cursomc/config/Initiable.java
+++ src/main/java/com/devdavi/cursomc/config/Initiable.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.devdavi.cursomc.domain.Categoria;
+import com.devdavi.cursomc.domain.Cidade;
+import com.devdavi.cursomc.domain.Estado;
 import com.devdavi.cursomc.domain.Produto;
 import com.devdavi.cursomc.repositories.CategoriaRepository;
+import com.devdavi.cursomc.repositories.CidadeRepository;
+import com.devdavi.cursomc.repositories.EstadoRepository;
 import com.devdavi.cursomc.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class Initiable implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,6 +43,20 @@ public class Initiable implements CommandLineRunner{
 		Produto p2 = new Produto(null, "Impressora", 800.00); 
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		
+		Estado e1 = new Estado(null, "Minas Gerais");
+		Estado e2 = new Estado(null, "São Paulo");
+		
+		
+		Cidade cid1 = new Cidade(null, "Uberlândia", e1);
+		Cidade cid2 = new Cidade(null, "São Paulo", e2);
+		Cidade cid3 = new Cidade(null, "Campinas", e2);
+		
+		e1.getCidades().add(cid1);
+		e2.getCidades().addAll(Arrays.asList(cid2,cid3));
+
+		estadoRepository.saveAll(Arrays.asList(e1,e2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
 		c1.getProdutos().addAll(Arrays.asList(p1,p2));
 		c2.getProdutos().addAll(Arrays.asList(p3));

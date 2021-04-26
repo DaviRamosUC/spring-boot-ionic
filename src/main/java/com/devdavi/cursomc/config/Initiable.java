17aa4db9df6d792375c7a5936958f98b26e80f2b
Davi <davi.lima@ucsal.edu.br>
2021-04-26 09:02:18 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/devdavi/cursomc/config/Initiable.java
+++ src/main/java/com/devdavi/cursomc/config/Initiable.java
@@ -13,6 +13,7 @@ import com.devdavi.cursomc.domain.Cidade;
 import com.devdavi.cursomc.domain.Cliente;
 import com.devdavi.cursomc.domain.Endereco;
 import com.devdavi.cursomc.domain.Estado;
+import com.devdavi.cursomc.domain.ItemPedido;
 import com.devdavi.cursomc.domain.Pagamento;
 import com.devdavi.cursomc.domain.PagamentoComBoleto;
 import com.devdavi.cursomc.domain.PagamentoComCartao;
@@ -25,20 +26,21 @@ import com.devdavi.cursomc.repositories.CidadeRepository;
 import com.devdavi.cursomc.repositories.ClienteRepository;
 import com.devdavi.cursomc.repositories.EnderecoRepository;
 import com.devdavi.cursomc.repositories.EstadoRepository;
+import com.devdavi.cursomc.repositories.ItemPedidoRepository;
 import com.devdavi.cursomc.repositories.PagamentoRepository;
 import com.devdavi.cursomc.repositories.PedidoRepository;
 import com.devdavi.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 @Profile("test")
-public class Initiable implements CommandLineRunner{
-	
+public class Initiable implements CommandLineRunner {
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
 
@@ -57,73 +59,85 @@ public class Initiable implements CommandLineRunner{
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
-		
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00); 
-		Produto p2 = new Produto(null, "Impressora", 800.00); 
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		
+
 		Estado e1 = new Estado(null, "Minas Gerais");
 		Estado e2 = new Estado(null, "São Paulo");
-		
-		
+
 		Cidade cid1 = new Cidade(null, "Uberlândia", e1);
 		Cidade cid2 = new Cidade(null, "São Paulo", e2);
 		Cidade cid3 = new Cidade(null, "Campinas", e2);
-		
-		e1.getCidades().add(cid1);
-		e2.getCidades().addAll(Arrays.asList(cid2,cid3));
 
-		estadoRepository.saveAll(Arrays.asList(e1,e2));
-		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
-		
-		c1.getProdutos().addAll(Arrays.asList(p1,p2));
+		e1.getCidades().add(cid1);
+		e2.getCidades().addAll(Arrays.asList(cid2, cid3));
+
+		estadoRepository.saveAll(Arrays.asList(e1, e2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+
+		c1.getProdutos().addAll(Arrays.asList(p1, p2));
 		c2.getProdutos().addAll(Arrays.asList(p3));
-		
+
 		p1.getCategorias().add(c1);
 		p2.getCategorias().add(c1);
 		p3.getCategorias().add(c2);
 
-		categoriaRepository.saveAll(Arrays.asList(c1,c2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-			
-		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
-		
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
 		Endereco end1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "38220834", cli1, cid1);
 		Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, cid2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
-		
+
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(end1,end2));
-		
+		enderecoRepository.saveAll(Arrays.asList(end1, end2));
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
+
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, end2);
-		
+
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		
+
 		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+
 		ped2.setPagamento(pagto2);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
-		
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
-	
-	
+
 }

3d5d91e7330cee92ba29f2cee86a48f1a78958aa
Davi <davi.lima@ucsal.edu.br>
2021-04-27 10:29:59 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/devdavi/cursomc/domain/DTO/ClienteDTO.java
+++ src/main/java/com/devdavi/cursomc/domain/DTO/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.devdavi.cursomc.domain.Cliente;
+import com.devdavi.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 

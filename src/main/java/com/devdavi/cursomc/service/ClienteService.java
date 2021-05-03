3eb13a89fbb4763583c37651b8511a5b7be8d7e3
Davi <davi.lima@ucsal.edu.br>
2021-05-03 20:05:07 -0300

    Enviando imagem via endpoint

--- src/main/java/com/devdavi/cursomc/service/ClienteService.java
+++ src/main/java/com/devdavi/cursomc/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.devdavi.cursomc.service;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.devdavi.cursomc.domain.Cidade;
 import com.devdavi.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public List<Cliente> findAll() {
 		List<Cliente> lista = repo.findAll();
@@ -109,4 +114,9 @@ public class ClienteService {
 		}
 		return cli;
 	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
 }
--- src/main/java/com/devdavi/cursomc/service/S3Service.java
+++ src/main/java/com/devdavi/cursomc/service/S3Service.java
@@ -1,41 +1,52 @@
 package com.devdavi.cursomc.service;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
 
 	private Logger LOG = org.slf4j.LoggerFactory.getLogger(S3Service.class);
-	
+
 	@Autowired
 	private AmazonS3 s3client;
-	
-	
+
 	@Value("${s3.bucket}")
 	private String bucketName;
-	
-	public void uploadFile(String localpath) {
+
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			File file = new File(localpath);
-			LOG.info("Iniciando upload");
-			s3client.putObject(new PutObjectRequest(bucketName, "teste", file));	
-			LOG.info("Upload finalizado");
-		}catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status code: "+ e.getErrorCode());
-		}catch (AmazonClientException err) {
-			LOG.info("AmazonClientException: " + err.getMessage());
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
 		}
-		
+	}
+
+	public URI uploadFile(InputStream is, String FileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			LOG.info("Iniciando upload");
+			s3client.putObject(bucketName, FileName, is, meta);
+			LOG.info("Upload finalizado");
+
+			return s3client.getUrl(bucketName, FileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao conveter URL para URI");
+		}
 	}
 }

ec354b6ad10eed0bae1dc5419790440c7558b82b
Davi <davi.lima@ucsal.edu.br>
2021-05-17 15:06:52 -0300

    Usando padrão de nomes para imagem

--- src/main/java/com/devdavi/cursomc/service/ClienteService.java
+++ src/main/java/com/devdavi/cursomc/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.devdavi.cursomc.service;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public List<Cliente> findAll() {
 		List<Cliente> lista = repo.findAll();
@@ -121,12 +129,10 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
-		Cliente cli = repo.findById(user.getId()).orElse(null);
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 	
 }

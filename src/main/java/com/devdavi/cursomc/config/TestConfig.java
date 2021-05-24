fb0a59a2a96fe442196ad63784348576867e7042
Davi <davi.lima@ucsal.edu.br>
2021-05-24 09:47:00 -0300

    Acrescentando mais produtos e alterando perfis com DbService

---
+++ src/main/java/com/devdavi/cursomc/config/TestConfig.java
@@ -0,0 +1,33 @@
+package com.devdavi.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.devdavi.cursomc.service.DBService;
+import com.devdavi.cursomc.service.EmailService;
+import com.devdavi.cursomc.service.MockEmailService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+
+	
+}
